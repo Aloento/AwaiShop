7f07da5a8f63c735e5e9c3997615cedfc38bbcb9
Aloento <11802769+Aloento@users.noreply.github.com>
2023-08-12 09:13:43 +0200

    Gallery

--- src/Pages/Gallery/index.tsx
+++ src/Pages/Gallery/index.tsx
@@ -1,3 +1,6 @@
+import { Divider, Title3, tokens } from "@fluentui/react-components";
+import { ColFlex } from "~/Helpers/Styles";
+
 /**
  * @author Aloento
  * @since 0.1.0
@@ -5,4 +8,21 @@
  */
 export function Gallery() {
 
+  return (
+    <div style={{
+      ...ColFlex,
+      columnGap: tokens.spacingVerticalL
+    }}>
+      <Title3>T-Shirt</Title3>
+
+      <div>
+
+      </div>
+
+      <Divider />
+
+
+
+    </div>
+  )
 }
