3a802ce8d590d162079c6000dab9cd0e6ba89057
左子祺 <evilzq@outlook.com>
2023-08-12 17:39:04 +0800

    Gallery Page

--- src/Pages/Gallery/index.tsx
+++ src/Pages/Gallery/index.tsx
@@ -1,27 +1,77 @@
-import { Divider, Title3, tokens } from "@fluentui/react-components";
-import { ColFlex } from "~/Helpers/Styles";
+import { Body1, Card, CardFooter, CardPreview, makeStyles, shorthands, tokens } from "@fluentui/react-components";
+import { Cover, Flex } from "~/Helpers/Styles";
+
+const useStyles = makeStyles({
+  card: {
+    marginTop: "10px",
+    marginBottom: "10px",
+    flexBasis: "23%",
+    flexGrow: 0,
+  },
+  img: {
+    aspectRatio: "1",
+    ...Cover,
+    ...shorthands.borderRadius(tokens.borderRadiusMedium)
+  },
+});
 
 /**
  * @author Aloento
  * @since 0.1.0
  * @version 0.1.0
  */
-export function Gallery() {
 
+class ProductInfo {
+  imageUrl: string
+  description: string
+  constructor() {
+    this.imageUrl = "https://source.unsplash.com/random";
+    this.description = "No Description";
+  }
+}
+
+export function Gallery() {
+  const styles = useStyles();
+  const cardList: ProductInfo[] = [
+    {
+      imageUrl: "https://source.unsplash.com/random",
+      description: "Product 1"
+    },
+    {
+      imageUrl: "https://source.unsplash.com/random",
+      description: "Product 2"
+    },
+    {
+      imageUrl: "https://source.unsplash.com/random",
+      description: "Product 3"
+    },
+    {
+      imageUrl: "https://source.unsplash.com/random",
+      description: "Product 4"
+    }
+  ]
   return (
     <div style={{
-      ...ColFlex,
+      ...Flex,
+      flexWrap: "wrap",
+      justifyContent: "space-evenly",
       columnGap: tokens.spacingVerticalL
     }}>
-      <Title3>T-Shirt</Title3>
+      {
+        cardList.map((item) => (
+          <Card className={styles.card}>
+            <CardPreview>
+              <img className={styles.img} src={item.imageUrl} />
+            </CardPreview>
 
-      <div>
-
-      </div>
-
-      <Divider />
-
-
+            <CardFooter>
+              <Body1>
+                {item.description}
+              </Body1>
+            </CardFooter>
+          </Card>
+        ))
+      }
 
     </div>
   )
