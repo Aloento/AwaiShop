b3ace46ed892cd215cce6acbe1b2fabdba1a7688
左子祺 <evilzq@outlook.com>
2023-08-13 15:55:32 +0800

    admin product list

--- src/Pages/Admin/Product/index.tsx
+++ src/Pages/Admin/Product/index.tsx
@@ -1,7 +1,155 @@
+import { Body1, Body1Strong, Button, DataGrid, DataGridBody, DataGridCell, DataGridHeader, DataGridHeaderCell, DataGridRow, Field, Image, TableColumnDefinition, createTableColumn, tokens } from "@fluentui/react-components";
+import { DeleteRegular } from "@fluentui/react-icons";
+import { ColFlex, Cover, Flex } from "~/Helpers/Styles";
+
+interface IProductItem {
+  Id: number;
+  Image: string;
+  Name: string;
+  Category: string,
+  Stock: number;
+}
+
+
+/**
+ * @author Aloento
+ * @since 0.1.0
+ * @version 0.1.0
+ */
+const columns: TableColumnDefinition<IProductItem>[] = [
+  createTableColumn<IProductItem>({
+    columnId: "Cover",
+    renderHeaderCell: () => {
+      return <div style={{ width: "44px" }} />;
+    },
+    renderCell(item) {
+      return <Image shape="square" style={{
+        ...Cover,
+        aspectRatio: "1",
+        height: "44px"
+      }} src={item.Image} />
+    },
+  }),
+  createTableColumn<IProductItem>({
+    columnId: "Product",
+    renderHeaderCell: () => {
+      return "Product";
+    },
+    renderCell(item) {
+      return (
+        <div style={ColFlex}>
+          <Body1Strong>{item.Name}</Body1Strong>
+        </div>
+      )
+    }
+  }),
+  createTableColumn<IProductItem>({
+    columnId: "Category",
+    renderHeaderCell: () => {
+      return "Category";
+    },
+    renderCell(item) {
+      return (
+        <Field defaultValue={item.Category}>
+          <Body1>{item.Category}</Body1>
+        </Field>
+      )
+    }
+  }),
+  createTableColumn<IProductItem>({
+    columnId: "Stock",
+    renderHeaderCell: () => {
+      return "Stock";
+    },
+    renderCell(item) {
+      return (
+        <Field defaultValue={item.Stock}>
+          <Body1>{item.Stock}</Body1>
+        </Field>
+      )
+    }
+  }),
+  createTableColumn<IProductItem>({
+    columnId: "Action",
+    renderHeaderCell: () => {
+      return "Delete";
+    },
+    renderCell(item) {
+      return <Button appearance="subtle" icon={<DeleteRegular />} />
+    },
+  })
+]
+
+const items: IProductItem[] = [
+  {
+    Id: 1,
+    Image: "https://picsum.photos/550",
+    Name: "OTC SHIRT - GREY",
+    Category:"Test",
+    Stock: 10,
+
+  },
+  {
+    Id: 2,
+    Image: "https://picsum.photos/600",
+    Name: "OTC Cap - Cap and Cap",
+    Category: "Test",
+    Stock: 10,
+  }
+]
+
+/**
+ * @author Aloento
+ * @since 0.1.0
+ * @version 0.1.0
+ */
 export function AdminProduct() {
   return (
-    <div>
+    <div style={{
+      ...ColFlex,
+      rowGap: tokens.spacingVerticalS
+    }}>
+      <div style={{
+        ...Flex,
+        justifyContent: "right"
+      }}>
+        <Button>Add New Product</Button>
+      </div>
+      <DataGrid
+        items={items}
+        columns={columns}
+        getRowId={(item: IProductItem) => item.Id}
+      >
+        <DataGridHeader>
+          <DataGridRow>
+            {({ columnId, renderHeaderCell }) => (
+              <DataGridHeaderCell style={{
+                flexBasis: "unset",
+                flexGrow: columnId === "Product" ? 1 : 0
+              }}>
+                {renderHeaderCell()}
+              </DataGridHeaderCell>
+            )}
+          </DataGridRow>
+        </DataGridHeader>
+
+        <DataGridBody<IProductItem>>
+          {({ item, rowId }) => (
+            <DataGridRow<IProductItem> key={rowId}>
+              {({ columnId, renderCell }) => (
+                <DataGridCell style={{
+                  flexBasis: "unset",
+                  flexGrow: columnId === "Product" ? 1 : 0
+                }}>
+                  {renderCell(item)}
+                </DataGridCell>
+              )}
+            </DataGridRow>
+          )}
+        </DataGridBody>
+      </DataGrid>
 
     </div>
   )
 }
+
