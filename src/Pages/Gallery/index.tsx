6e00243de7e71500f40c2e4b5c56524952452357
Aloento <11802769+Aloento@users.noreply.github.com>
2023-09-03 16:36:26 +0200

    Gallery

--- src/Pages/Gallery/index.tsx
+++ src/Pages/Gallery/index.tsx
@@ -1,5 +1,7 @@
 import { Body1, Card, CardFooter, CardPreview, Image, Link, Title3, makeStyles, tokens } from "@fluentui/react-components";
+import { useAsyncMemo } from "~/Helpers/AsyncMemo";
 import { ColFlex, Cover, Flex } from "~/Helpers/Styles";
+import { Hub } from "~/ShopNet";
 
 /**
  * @author Aloento
@@ -17,6 +19,17 @@ const useStyles = makeStyles({
     borderTopLeftRadius: tokens.borderRadiusMedium,
     borderTopRightRadius: tokens.borderRadiusMedium,
   },
+  cate: {
+    ...Flex,
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    columnGap: tokens.spacingVerticalL,
+    rowGap: tokens.spacingVerticalXL
+  },
+  main: {
+    ...ColFlex,
+    rowGap: tokens.spacingVerticalXL
+  }
 });
 
 /**
@@ -24,29 +37,10 @@ const useStyles = makeStyles({
  * @since 0.1.0
  * @version 0.1.0
  */
-interface ProductInfo {
+export interface ProductInfo {
   Image: string;
   Name: string;
 }
-
-const cardList: ProductInfo[] = [
-  {
-    Image: "https://source.unsplash.com/random",
-    Name: "Product 1"
-  },
-  {
-    Image: "https://source.unsplash.com/random",
-    Name: "Product 2"
-  },
-  {
-    Image: "https://source.unsplash.com/random",
-    Name: "Product 3"
-  },
-  {
-    Image: "https://source.unsplash.com/random",
-    Name: "Product 4"
-  }
-]
 
 /**
  * @author Aloento
@@ -55,39 +49,58 @@ const cardList: ProductInfo[] = [
  */
 export function Gallery() {
   const style = useStyles();
+  const cates = useAsyncMemo(Hub.Gallery.Get.Categories(), [], []);
 
   return (
-    <div style={{
-      ...ColFlex,
-      rowGap: tokens.spacingVerticalXL
-    }}>
-      <Title3>T-Shirt</Title3>
-
-      <div style={{
-        ...Flex,
-        flexWrap: "wrap",
-        justifyContent: "space-evenly",
-        columnGap: tokens.spacingVerticalL,
-        rowGap: tokens.spacingVerticalXL
-      }}>
-        {
-          cardList.map((item) => (
-            <Card className={style.card}>
-              <CardPreview>
-                <Image className={style.img} src={item.Image} />
-              </CardPreview>
-
-              <CardFooter>
-                <Body1>
-                  <Link href="/Product">{item.Name}</Link>
-                </Body1>
-              </CardFooter>
-            </Card>
-          ))
-        }
-      </div>
-
-      <Title3>Cap</Title3>
+    <div className={style.main}>
+      {
+        cates.map(x => <GalleryCategory Category={x} />)
+      }
     </div>
+  )
+}
+
+/**
+ * @author Aloento
+ * @since 0.5.0
+ * @version 0.1.0
+ */
+function GalleryCategory({ Category }: { Category: string }) {
+  const style = useStyles();
+  const list = useAsyncMemo(Hub.Gallery.Get.Products(Category), [Category], [[], 0]);
+
+  return <>
+    <Title3>{Category}</Title3>
+
+    <div className={style.cate}>
+      {
+        list[0].map(x => <GalleryCard Id={x} />)
+          .concat(Array(list[1]).fill(<div className={style.card} />))
+      }
+    </div>
+  </>
+}
+
+/**
+ * @author Aloento
+ * @since 0.5.0
+ * @version 0.1.0
+ */
+function GalleryCard({ Id }: { Id: number }) {
+  const style = useStyles();
+  const info = useAsyncMemo(Hub.Gallery.Get.Basic(Id), [Id])
+
+  return (
+    <Card className={style.card}>
+      <CardPreview>
+        <Image className={style.img} src={info?.Image} />
+      </CardPreview>
+
+      <CardFooter>
+        <Body1>
+          <Link href={`/Product/${Id}`}>{info?.Name}</Link>
+        </Body1>
+      </CardFooter>
+    </Card>
   )
 }
--- src/ShopNet/Gallery/Get.ts
+++ src/ShopNet/Gallery/Get.ts
@@ -1,3 +1,6 @@
+import { random } from "lodash-es";
+import { ProductInfo } from "~/Pages/Gallery";
+
 /**
  * @author Aloento
  * @since 0.5.0
@@ -9,9 +12,37 @@ export class GalleryGet {
    * @since 0.5.0
    * @version 0.1.0
    */
-  public static async Categories(): Promise<{}> {
-    return {
+  public static async Categories(): Promise<string[]> {
+    return [
+      "T-Shirt",
+      "Cap"
+    ];
+  }
 
-    };
+  /**
+   * @author Aloento
+   * @since 0.5.0
+   * @version 0.1.0
+   */
+  public static async Products(category: string): Promise<[number[], number]> {
+    const len = random(1, 12);
+    const nums: number[] = Array(len).fill(0).map(() => random(1, 100));
+
+    return [
+      nums,
+      4 - (len % 4)
+    ]
+  }
+
+  /**
+   * @author Aloento
+   * @since 0.5.0
+   * @version 0.1.0
+   */
+  public static async Basic(id: number): Promise<ProductInfo> {
+    return {
+      Image: "https://source.unsplash.com/random",
+      Name: `Product ${id}`
+    }
   }
 }
