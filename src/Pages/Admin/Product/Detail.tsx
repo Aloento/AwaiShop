11fe533436b8bacd61838fbf0d823308a99cee7d
Aloento <11802769+Aloento@users.noreply.github.com>
2023-10-04 16:55:18 +0200

    ProductName

--- src/Pages/Admin/Product/Detail.tsx
+++ src/Pages/Admin/Product/Detail.tsx
@@ -1,12 +1,10 @@
-import { Button, Input, Subtitle2, Toast, ToastTitle, makeStyles, tokens } from "@fluentui/react-components";
+import { Button, makeStyles, tokens } from "@fluentui/react-components";
 import { Drawer, DrawerBody, DrawerHeader, DrawerHeaderTitle } from "@fluentui/react-components/unstable";
-import { DismissRegular, EditRegular, OpenRegular, SendRegular } from "@fluentui/react-icons";
-import { useBoolean, useRequest } from "ahooks";
-import { useState } from "react";
+import { DismissRegular, OpenRegular } from "@fluentui/react-icons";
+import { useBoolean } from "ahooks";
 import { ColFlex } from "~/Helpers/Styles";
-import { use500Toast } from "~/Helpers/useToast";
-import { AdminHub } from "~/ShopNet/Admin";
 import { AdminProductCombo } from "./Combo";
+import { ProductName } from "./Name";
 import { AdminProductPhoto } from "./Photo";
 import { AdminProductVariant } from "./Variant";
 
@@ -29,40 +27,7 @@ const useStyles = makeStyles({
  */
 export function AdminProductDetail({ ProdId }: { ProdId: number }) {
   const style = useStyles();
-  const [name, setName] = useState("");
-
-  const [edit, { setTrue, setFalse }] = useBoolean();
   const [open, { toggle }] = useBoolean();
-
-  useRequest(AdminHub.Product.Get.Name, {
-    defaultParams: [ProdId],
-    onSuccess(data) {
-      setName(data);
-    },
-  })
-
-  const { dispatchError, dispatchToast } = use500Toast();
-
-  const { run } = useRequest(AdminHub.Product.Post.Name, {
-    manual: true,
-    onFinally([req], _, e) {
-      if (e)
-        dispatchError({
-          Message: "Failed Update Name",
-          Request: req,
-          Error: e
-        });
-
-      dispatchToast(
-        <Toast>
-          <ToastTitle>Name Updated</ToastTitle>
-        </Toast>,
-        { intent: "success" }
-      );
-
-      setFalse();
-    },
-  });
 
   return <>
     <Button
@@ -91,18 +56,7 @@ export function AdminProductDetail({ ProdId }: { ProdId: number }) {
       </DrawerHeader>
 
       <DrawerBody className={style.body}>
-        <Input
-          size="large"
-          value={name}
-          disabled={!edit}
-          appearance="underline"
-          onChange={(_, v) => setName(v.value)}
-          contentBefore={<Subtitle2>Name</Subtitle2>}
-          contentAfter={edit
-            ? <Button appearance="subtle" icon={<SendRegular />} onClick={() => run(ProdId, name)} />
-            : <Button appearance="subtle" icon={<EditRegular />} onClick={setTrue} />}
-        />
-
+        <ProductName ProdId={ProdId} />
         <AdminProductPhoto ProdId={ProdId} />
         <AdminProductVariant ProdId={ProdId} />
         <AdminProductCombo ProdId={ProdId} />
