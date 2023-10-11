3244cb2ed37c572afcf5203333fdcb29bd75a3a5
Aloento <11802769+Aloento@users.noreply.github.com>
2023-10-11 20:51:39 +0200

    GalleryGet

---
+++ SoarCraft.LoveOTC/Hub/Gallery/Get.cs
@@ -0,0 +1,46 @@
+namespace SoarCraft.LoveOTC.Hub;
+
+using JetBrains.Annotations;
+
+/**
+ * <remarks>
+ * @author Aloento
+ * @since 0.1.0
+ * @version 0.1.0
+ * </remarks>
+ */
+internal partial class ShopHub {
+    /**
+     * <remarks>
+     * @author Aloento
+     * @since 0.1.0
+     * @version 0.1.0
+     * </remarks>
+     */
+    [PublicAPI]
+    public async Task<List<string>> Categories() {
+        return new(){
+            "T-Shirt",
+            "Cap"
+        };
+    }
+
+    /**
+     * <remarks>
+     * @author Aloento
+     * @since 0.1.0
+     * @version 0.1.0
+     * </remarks>
+     */
+    [PublicAPI]
+    public async Task<List<uint>> Products(string category) { 
+        var len = Random.Shared.Next(1, 12); 
+        var nums = new List<uint>(len);
+
+        for (var i = 0; i < len; i++) {
+            nums.Add((uint) Random.Shared.Next(1, 100));
+        }
+
+        return nums;
+    }
+}
--- src/ShopNet/ShopNet.ts
+++ src/ShopNet/ShopNet.ts
@@ -1,4 +1,4 @@
-import { HttpTransportType, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { HttpTransportType, HubConnectionBuilder, HubConnectionState, LogLevel } from "@microsoft/signalr";
 import { MessagePackHubProtocol } from "@microsoft/signalr-protocol-msgpack";
 import { User } from "oidc-client-ts";
 import { OIDC } from "./Database";
@@ -23,6 +23,11 @@ export function accessTokenFactory(): string {
  * @version 0.1.0
  */
 export class ShopNet {
+  /**
+   * @author Aloento
+   * @since 1.0.0
+   * @version 0.1.0
+   */
   public static readonly Hub = new HubConnectionBuilder()
     .withUrl(import.meta.env.DEV ? "https://localhost/Hub" : "/Hub",
       {
@@ -35,4 +40,16 @@ export class ShopNet {
     .withHubProtocol(new MessagePackHubProtocol())
     .configureLogging(import.meta.env.DEV ? LogLevel.Debug : LogLevel.Information)
     .build();
+
+  /**
+   * @author Aloento
+   * @since 1.0.0
+   * @version 0.1.0
+   */
+  public static async EnsureConnected() {
+    if (this.Hub.state === HubConnectionState.Disconnected
+      || this.Hub.state === HubConnectionState.Disconnecting) {
+      await this.Hub.start();
+    }
+  }
 }
