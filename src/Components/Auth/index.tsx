efc5e76af1ea57093e9d96125d035629e0f5c7de
Aloento <11802769+Aloento@users.noreply.github.com>
2023-10-11 11:30:08 +0200

    try

--- src/Components/Auth/index.tsx
+++ src/Components/Auth/index.tsx
@@ -18,10 +18,6 @@ export function OIDCProvider({ children }: { children: ReactNode }): ReactNode {
       authority="https://keycloak.eco.tsi-dev.otc-service.com/realms/eco"
       client_id="loveotc"
       scope="openid profile email loveotc"
-      extraTokenParams={{
-        client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-        client_assertion: "eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlNzBjNmE1ZS0wMzgyLTQxNzAtOGE0Ny1iYzQyZmU4NWM0M2YifQ.eyJleHAiOjAsImlhdCI6MTY5Njk1NDIwNCwianRpIjoiYWZlNTMzMDYtNDdjZi00MTMwLWI5NDctYjU5MmNlNzNkNTRhIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5lY28udHNpLWRldi5vdGMtc2VydmljZS5jb20vcmVhbG1zL2VjbyIsImF1ZCI6Imh0dHBzOi8va2V5Y2xvYWsuZWNvLnRzaS1kZXYub3RjLXNlcnZpY2UuY29tL3JlYWxtcy9lY28iLCJ0eXAiOiJSZWdpc3RyYXRpb25BY2Nlc3NUb2tlbiIsInJlZ2lzdHJhdGlvbl9hdXRoIjoiYXV0aGVudGljYXRlZCJ9.ZxA92m0X0alA04Pwmh68-kAUCgoMAhqP3a6Eb26P-nA"
-      }}
       redirect_uri="https://shop.eco.tsi-dev.otc-service.com/oauth2/callback"
       userStore={new WebStorageStateStore({ store: window.localStorage })}
       onSigninCallback={() => Rep("/")}
