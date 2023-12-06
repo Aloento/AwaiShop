dac6339bd3150884cf875386e4a1a13842d12818
Aloento <11802769+Aloento@users.noreply.github.com>
2023-12-06 21:06:23 +0100

    Update admin user grant functionality

--- src/Pages/Admin/User/Admin.tsx
+++ src/Pages/Admin/User/Admin.tsx
@@ -6,21 +6,21 @@ import { AdminHub } from "~/ShopNet/Admin";
 /**
  * @author Aloento
  * @since 0.5.0
- * @version 0.1.0
+ * @version 0.1.1
  */
 export function AdminUserAdmin({ UserId, Admin, Refresh }: { UserId: string; Admin?: boolean; Refresh: () => void }) {
   const { dispatch, dispatchToast } = useErrorToast();
 
-  const { run: grant } = useRequest(AdminHub.User.Post.Admin.bind(AdminHub.User.Post), {
+  const { run: grant } = AdminHub.User.Post.Admin({
     manual: true,
-    onFinally(req, _, e) {
-      if (e)
-        return dispatch({
-          Message: "Failed Grant Admin",
-          Request: req,
-          Error: e
-        });
-
+    onError(e, req) {
+      dispatch({
+        Message: "Failed Grant Admin",
+        Request: req,
+        Error: e
+      });
+    },
+    onSuccess() {
       dispatchToast(
         <Toast>
           <ToastTitle>Admin Granted</ToastTitle>
@@ -29,7 +29,7 @@ export function AdminUserAdmin({ UserId, Admin, Refresh }: { UserId: string; Adm
       );
 
       Refresh();
-    },
+    }
   });
 
   const { run: revoke } = useRequest(AdminHub.User.Delete.Admin.bind(AdminHub.User.Delete), {
