b54059a050330249ac7a54f9a8f3c26d23635d7f
Aloento <11802769+Aloento@users.noreply.github.com>
2023-12-10 17:33:17 +0100

    Add new methods and rename test method in Product/Get.cs

--- SoarCraft.AwaiShop.Test/Hub/Product/Get.cs
+++ SoarCraft.AwaiShop.Test/Hub/Product/Get.cs
@@ -6,6 +6,24 @@ using Microsoft.AspNetCore.SignalR.Client;
 [TestCategory("Hub.Product.Get")]
 public class Get : ShopNet
 {
+    public static Task<T> ProductEntity<T>(uint key, byte[]? version) =>
+        Guest.InvokeAsync<T>(nameof(ProductEntity), key, version);
+
+    public static Task<T> PhotoEntity<T>(uint key, byte[]? version) =>
+        Guest.InvokeAsync<T>(nameof(PhotoEntity), key, version);
+
+    public static Task<T> TypeEntity<T>(uint key, byte[]? version) =>
+        Guest.InvokeAsync<T>(nameof(TypeEntity), key, version);
+
+    public static Task<T> VariantEntity<T>(uint key, byte[]? version) =>
+        Guest.InvokeAsync<T>(nameof(VariantEntity), key, version);
+
+    public static Task<dynamic[]> ProductGetComboList(uint prodId) =>
+        Guest.InvokeAsync<dynamic[]>(nameof(ProductGetComboList), prodId);
+
+    public static Task<uint[]> ProductGetPhotoList(uint prodId) =>
+        Guest.InvokeAsync<uint[]>(nameof(ProductGetPhotoList), prodId);
+
     /**
      * <remarks>
      * @Author Aloento
@@ -13,7 +31,7 @@ public class Get : ShopNet
      * </remarks>
      */
     [TestMethod]
-    public async Task ProductGetComboList()
+    public async Task ComboList()
     {
         _ = await Guest.InvokeAsync<Dictionary<string, dynamic>[]>(nameof(ProductGetComboList), 0);
     }
