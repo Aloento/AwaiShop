ee7df2b644d9e3105b57fc39b785174390056522
Aloento <Aloento@users.noreply.github.com>
2024-01-13 23:54:03 +0000

    deploy: 119244c35c599139d173c81199dda4a4f98116fb

---
+++ assets/Lazy-o4zom_ZO.js
@@ -0,0 +1,7 @@
+import{r as a,as as e,j as s,ar as t}from"./vendor-HDueFIKT.js";function i(r){return s.jsx(a.Suspense,{fallback:s.jsx(t,{}),children:s.jsx(n,{...r})})}const n=a.lazy(()=>e(()=>import("./index-qnI-IQJp.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{i as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-qnI-IQJp.js","assets/vendor-HDueFIKT.js","assets/index-649eOVXq.js","assets/index-mRiAC1cH.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
