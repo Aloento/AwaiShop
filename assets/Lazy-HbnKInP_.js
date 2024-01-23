31e238fa67bc4b09ba5367711aed8bb7527a8990
Aloento <Aloento@users.noreply.github.com>
2024-01-23 22:19:28 +0000

    deploy: ef453edf4d9a99e7e6d7cf0b39182da512fbe8f7

---
+++ assets/Lazy-HbnKInP_.js
@@ -0,0 +1,7 @@
+import{r as a,as as e,j as s,ar as t}from"./vendor-EPO6t9z0.js";function i(r){return s.jsx(a.Suspense,{fallback:s.jsx(t,{}),children:s.jsx(n,{...r})})}const n=a.lazy(()=>e(()=>import("./index-9pHM_LcE.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{i as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-9pHM_LcE.js","assets/vendor-EPO6t9z0.js","assets/index-q7rnA42-.js","assets/index-mRiAC1cH.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
