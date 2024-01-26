a084b6a5222fd5fa6588c7a6818f45433671551d
Aloento <Aloento@users.noreply.github.com>
2024-01-26 19:56:33 +0000

    deploy: b8f413c15dce6ae93a9effa780f3a8f0e7a00841

---
+++ assets/index--3fLyjhp.js
@@ -0,0 +1,1 @@
+import{x as o,j as t,y as e}from"./vendor-d5bvWNts.js";import{L as a,H as n,f as i}from"./index-CIi-pVKZ.js";import{H as m}from"./Columns-pt6hKff7.js";const s=new a("History");function l(){const{data:r}=o(()=>n.Order.Get.List(s),{onError:s.error});return t.jsx(i,{Items:r,Columns:e(()=>m(s))})}export{l as default};
