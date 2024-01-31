47a5642a404e89582c847b1dcd6d0fc7c25368d1
Aloento <Aloento@users.noreply.github.com>
2024-01-31 12:38:25 +0000

    deploy: 1d4204ff7421fa9205896095e1bbb8c821076981

--- assets/index-QtnnZT98.js
+++
@@ -1,1 +0,0 @@
-import{x as o,j as t,y as e}from"./vendor-VMat-HGY.js";import{L as a,H as n,f as i}from"./index-6uh04jNc.js";import{H as m}from"./Columns-2WbzxjVt.js";const s=new a("History");function l(){const{data:r}=o(()=>n.Order.Get.List(s),{onError:s.error});return t.jsx(i,{Items:r,Columns:e(()=>m(s))})}export{l as default};
