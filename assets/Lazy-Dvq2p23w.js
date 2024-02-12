19e1324ad2c731139ae9c20a564d2355e3f1e989
Aloento <Aloento@users.noreply.github.com>
2024-02-12 12:57:54 +0000

    deploy: eb85398d134d4681b3f05cb313b1a792587d2d84

---
+++ assets/Lazy-Dvq2p23w.js
@@ -0,0 +1,7 @@
+import{_ as o}from"./vendor-Cg4l60Qw.js";import{j as t,a7 as s}from"./components-P3HMgWxx.js";import{r as e}from"./react-mhcLWgJf.js";function m(r){return t.jsx(e.Suspense,{fallback:t.jsx(s,{}),children:t.jsx(a,{...r})})}const a=e.lazy(()=>o(()=>import("./index-D7-8M7Y0.js").then(r=>r.l),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10])));export{m as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-D7-8M7Y0.js","assets/components-P3HMgWxx.js","assets/react-mhcLWgJf.js","assets/fluentui-D10euT8-.js","assets/lexical-jWKiw7TX.js","assets/index-DQ4A6yrT.js","assets/vendor-Cg4l60Qw.js","assets/tools-BeP_WXzy.js","assets/rx-BIoNU6Ta.js","assets/signalr-wWpYR44r.js","assets/index-CZGIALVw.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
