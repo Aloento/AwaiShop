a081f6c0408cb81e7176d5db81bb976b725c09b7
Aloento <11802769+Aloento@users.noreply.github.com>
2024-02-12 16:05:57 +0100

    Update vite.config.ts with splitVendorChunkPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
-import pack from "./package.json";
-
-const deps = Object.keys(pack.dependencies).filter(x => !x.includes("@lexical/react")) as (keyof typeof pack.dependencies)[];
-
-const map = deps.reduce((acc, key) => {
-  acc[key] = key;
-  return acc;
-}, {} as Record<keyof typeof pack.dependencies, string>);
-
-function get(name: keyof typeof pack.dependencies) {
-  const result = map[name];
-  delete map[name];
-  return result;
-}
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin()
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
@@ -29,20 +18,6 @@ export default defineConfig({
     reportCompressedSize: false,
     modulePreload: {
       polyfill: false,
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: [get("react"), get("react-dom")],
-          lexical: deps.filter(x => x.includes("lexical")).map(x => get(x)),
-          components: [get("@fluentui/react-components")],
-          fluentui: [get("@fluentui/react-icons"), get("@fluentui/react-hooks"), get("@griffel/react")],
-          signalr: [get("@microsoft/signalr"), get("@microsoft/signalr-protocol-msgpack")],
-          rx: [get("dexie"), get("dexie-react-hooks"), get("rxjs")],
-          tools: [get("ahooks"), get("dayjs"), get("lodash-es"), get("react-helmet"), get("react-responsive-carousel")],
-          vendor: Object.values(map),
-        },
-      },
     }
   },
 });
