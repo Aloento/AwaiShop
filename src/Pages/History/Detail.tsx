b2dcd7a14a45c7bbfd4ca1a4cac247186babfc11
Aloento <11802769+Aloento@users.noreply.github.com>
2024-02-13 20:28:13 +0100

    Refactor OrderDetail component and remove unused code

--- src/Pages/History/Detail.tsx
+++ src/Pages/History/Detail.tsx
@@ -1,81 +1,14 @@
-import { Body1Strong, Button, Caption1, DataGridCell, DataGridHeaderCell, Link, SkeletonItem, TableColumnDefinition, createTableColumn, makeStyles, tokens } from "@fluentui/react-components";
+import { Button } from "@fluentui/react-components";
 import { Drawer, DrawerBody, DrawerHeader, DrawerHeaderTitle } from "@fluentui/react-components/unstable";
 import { useConst } from "@fluentui/react-hooks";
 import { DismissRegular, OpenRegular } from "@fluentui/react-icons";
-import { useBoolean, useInViewport, useRequest } from "ahooks";
+import { useBoolean, useInViewport } from "ahooks";
 import { useEffect, useRef } from "react";
-import { DelegateDataGrid } from "~/Components/DataGrid";
-import { IComment, OrderComment } from "~/Components/Order/Comment";
-import { OrderInfo } from "~/Components/OrderInfo";
+import { IComment } from "~/Components/Order/Comment";
 import { useRouter } from "~/Components/Router";
 import { ICartItem } from "~/Components/ShopCart";
-import { MakeCoverCol } from "~/Helpers/CoverCol";
 import { ICompLog } from "~/Helpers/Logger";
-import { ColFlex } from "~/Helpers/Styles";
-import { Hub } from "~/ShopNet";
-import { OrderAction } from "./Action";
-
-/**
- * @author Aloento
- * @since 0.5.0
- * @version 0.1.0
- */
-const useStyles = makeStyles({
-  body: {
-    ...ColFlex,
-    rowGap: tokens.spacingVerticalL
-  },
-  prod: {
-    ...ColFlex,
-    alignItems: "flex-start",
-    justifyContent: "center",
-  },
-  qua: {
-    flexBasis: "10%",
-    flexGrow: 0,
-    justifyContent: "center"
-  },
-});
-
-/**
- * @author Aloento
- * @since 0.5.0
- * @version 0.1.1
- */
-const columns: TableColumnDefinition<ICartItem>[] = [
-  createTableColumn<ICartItem>({
-    columnId: "Product",
-    renderHeaderCell() {
-      return <DataGridHeaderCell>Product Name & Types</DataGridHeaderCell>;
-    },
-    renderCell(item) {
-      return (
-        <DataGridCell className={useStyles().prod}>
-          <Link href={`/Product/${item.ProdId}`} appearance="subtle">
-            <Body1Strong>{item.Name}</Body1Strong>
-          </Link>
-
-          <Caption1>
-            {Object.values(item.Type).reduce((prev, curr) => `${prev} ${curr},`, "")}
-          </Caption1>
-        </DataGridCell>
-      );
-    }
-  }),
-  createTableColumn<ICartItem>({
-    columnId: "Quantity",
-    renderHeaderCell() {
-      return <DataGridHeaderCell className={useStyles().qua}>Quantity</DataGridHeaderCell>;
-    },
-    renderCell(item) {
-      return (
-        <DataGridCell className={useStyles().qua}>
-          {item.Quantity}
-        </DataGridCell>
-      );
-    }
-  })
-];
+import { OrderDetailDrawer } from "./Drawer";
 
 /**
  * @author Aloento
@@ -90,7 +23,7 @@ export interface IOrderDetail {
 /**
  * @author Aloento
  * @since 0.5.0
- * @version 1.0.0
+ * @version 1.1.0
  */
 export function OrderDetail({ OrderId, ParentLog }: { OrderId: number } & ICompLog) {
   const log = useConst(() => ParentLog.With("Detail"));
@@ -131,50 +64,8 @@ export function OrderDetail({ OrderId, ParentLog }: { OrderId: number } & ICompL
       </DrawerHeader>
 
       <DrawerBody ref={ref}>
-        {inViewport && <DeferredBody OrderId={OrderId} ParentLog={log} />}
+        {inViewport && <OrderDetailDrawer OrderId={OrderId} ParentLog={log} />}
       </DrawerBody>
     </Drawer>
   </>
-}
-
-/**
- * @author Aloento
- * @since 1.3.5
- * @version 0.1.0
- */
-function DeferredBody({ OrderId, ParentLog }: { OrderId: number } & ICompLog) {
-  const style = useStyles();
-  const { Nav } = useRouter();
-
-  const { data: order, run: runOrder } = useRequest(() => Hub.Order.Get.Order(OrderId), {
-    onError(e) {
-      Nav("History");
-      ParentLog.error(e);
-    },
-    manual: true
-  });
-
-  const { data: cart, run: runItems, loading } = Hub.Order.Get.useItems(OrderId, ParentLog);
-
-  const run = () => {
-    runItems();
-    runOrder();
-  };
-
-  return (
-    <div className={style.body}>
-      <OrderInfo OrderId={OrderId} Order={order} />
-
-      <DelegateDataGrid
-        Items={cart}
-        Columns={[MakeCoverCol(44, ParentLog), ...columns]}
-      />
-
-      {loading && <SkeletonItem size={48} />}
-
-      <OrderComment OrderId={OrderId} Status={order?.Status} Refresh={run} ParentLog={ParentLog} />
-
-      <OrderAction OrderId={OrderId} Status={order?.Status} Refresh={run} ParentLog={ParentLog} />
-    </div>
-  );
 }
---
+++ src/Pages/History/Drawer.tsx
@@ -0,0 +1,116 @@
+import { Body1Strong, Caption1, DataGridCell, DataGridHeaderCell, Link, SkeletonItem, TableColumnDefinition, createTableColumn, makeStyles, tokens } from "@fluentui/react-components";
+import { useRequest } from "ahooks";
+import { DelegateDataGrid } from "~/Components/DataGrid";
+import { OrderComment } from "~/Components/Order/Comment";
+import { OrderInfo } from "~/Components/OrderInfo";
+import { useRouter } from "~/Components/Router";
+import { ICartItem } from "~/Components/ShopCart";
+import { MakeCoverCol } from "~/Helpers/CoverCol";
+import { ICompLog } from "~/Helpers/Logger";
+import { ColFlex } from "~/Helpers/Styles";
+import { Hub } from "~/ShopNet";
+import { OrderAction } from "./Action";
+
+/**
+ * @author Aloento
+ * @since 0.5.0
+ * @version 0.1.0
+ */
+const useStyles = makeStyles({
+  body: {
+    ...ColFlex,
+    rowGap: tokens.spacingVerticalL
+  },
+  prod: {
+    ...ColFlex,
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  qua: {
+    flexBasis: "10%",
+    flexGrow: 0,
+    justifyContent: "center"
+  },
+});
+
+/**
+ * @author Aloento
+ * @since 0.5.0
+ * @version 0.1.1
+ */
+const columns: TableColumnDefinition<ICartItem>[] = [
+  createTableColumn<ICartItem>({
+    columnId: "Product",
+    renderHeaderCell() {
+      return <DataGridHeaderCell>Product Name & Types</DataGridHeaderCell>;
+    },
+    renderCell(item) {
+      return (
+        <DataGridCell className={useStyles().prod}>
+          <Link href={`/Product/${item.ProdId}`} appearance="subtle">
+            <Body1Strong>{item.Name}</Body1Strong>
+          </Link>
+
+          <Caption1>
+            {Object.values(item.Type).reduce((prev, curr) => `${prev} ${curr},`, "")}
+          </Caption1>
+        </DataGridCell>
+      );
+    }
+  }),
+  createTableColumn<ICartItem>({
+    columnId: "Quantity",
+    renderHeaderCell() {
+      return <DataGridHeaderCell className={useStyles().qua}>Quantity</DataGridHeaderCell>;
+    },
+    renderCell(item) {
+      return (
+        <DataGridCell className={useStyles().qua}>
+          {item.Quantity}
+        </DataGridCell>
+      );
+    }
+  })
+];
+
+/**
+ * @author Aloento
+ * @since 1.3.5
+ * @version 1.0.0
+ */
+export function OrderDetailDrawer({ OrderId, ParentLog }: { OrderId: number } & ICompLog) {
+  const style = useStyles();
+  const { Nav } = useRouter();
+
+  const { data: order, run: runOrder } = useRequest(() => Hub.Order.Get.Order(OrderId), {
+    onError(e) {
+      Nav("History");
+      ParentLog.error(e);
+    },
+    manual: true
+  });
+
+  const { data: cart, run: runItems, loading } = Hub.Order.Get.useItems(OrderId, ParentLog);
+
+  const run = () => {
+    runItems();
+    runOrder();
+  };
+
+  return (
+    <div className={style.body}>
+      <OrderInfo OrderId={OrderId} Order={order} />
+
+      <DelegateDataGrid
+        Items={cart}
+        Columns={[MakeCoverCol(44, ParentLog), ...columns]}
+      />
+
+      {loading && <SkeletonItem size={48} />}
+
+      <OrderComment OrderId={OrderId} Status={order?.Status} Refresh={run} ParentLog={ParentLog} />
+
+      <OrderAction OrderId={OrderId} Status={order?.Status} Refresh={run} ParentLog={ParentLog} />
+    </div>
+  );
+}
