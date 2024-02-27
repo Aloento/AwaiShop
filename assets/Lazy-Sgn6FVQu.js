0833170d436c32cc2c6b756524359de94b6d3a45
Aloento <Aloento@users.noreply.github.com>
2024-02-27 18:43:21 +0000

    deploy: d313d33da5690fef464f5e404ac88128a972a263

--- assets/Lazy-Sgn6FVQu.js
+++
@@ -1,7 +0,0 @@
-import{r as e,av as s,j as a,au as t}from"./vendor-C9L1vlTP.js";function i(r){return a.jsx(e.Suspense,{fallback:a.jsx(t,{}),children:a.jsx(n,{...r})})}const n=e.lazy(()=>s(()=>import("./index-CLGaJlNZ.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{i as L};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-CLGaJlNZ.js","assets/vendor-C9L1vlTP.js","assets/index-BdrHVpum.js","assets/index-CZGIALVw.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
