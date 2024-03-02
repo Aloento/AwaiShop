aefc98502e6f86bd632dc1b2bb53239130cf7800
Aloento <Aloento@users.noreply.github.com>
2024-03-02 17:18:55 +0000

    deploy: 0413523f453977f803aaebb2fe600795e2942156

---
+++ assets/Lazy-BSnUzjDz.js
@@ -0,0 +1,7 @@
+import{r as e,av as s,j as a,au as t}from"./vendor-BRd9sncS.js";function i(r){return a.jsx(e.Suspense,{fallback:a.jsx(t,{}),children:a.jsx(n,{...r})})}const n=e.lazy(()=>s(()=>import("./index-CH2o-s3o.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{i as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-CH2o-s3o.js","assets/vendor-BRd9sncS.js","assets/index-CPdKjF50.js","assets/index-CZGIALVw.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- assets/Lazy-CrHFBS1-.js
+++
@@ -1,7 +0,0 @@
-import{r as e,av as s,j as a,au as t}from"./vendor-BRd9sncS.js";function i(r){return a.jsx(e.Suspense,{fallback:a.jsx(t,{}),children:a.jsx(n,{...r})})}const n=e.lazy(()=>s(()=>import("./index-CO7TjBQ3.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{i as L};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-CO7TjBQ3.js","assets/vendor-BRd9sncS.js","assets/index-N-KoVd_J.js","assets/index-CZGIALVw.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
